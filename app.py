1c4866263c33ed06a97ab92a33e89bc31e74b664
suhwan-cheon <soo7652@naver.com>
2023-05-14 00:39:22 +0900

    yfinance 라이브러리 설치 및 requirements.txt 추가

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+import yfinance as yf
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template('./index.html')
+
+@app.route("/get_stock")
+def get_stock():
+    msft = yf.Ticker("MSFT")
+
+    # get all stock info
+    data = msft.info
+    print(data)
     return render_template('./index.html')
