03f50aecd8c663227e9573cbd45d359d46ac6dea
stephen-c <stephen.c@kakaocorp.com>
2023-06-14 23:23:04 +0900

    kafka 속성 정보 및 라이브 스트림 넣는 함수 호출 부분 추가

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
+from dotenv import load_dotenv
+import os
+import pickle
 from flask import Flask, render_template
 import yfinance as yf
-# from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, timedelta
+from confluent_kafka import Producer
 
 app = Flask(__name__)
+
+load_dotenv(".env")
+
+# 토픽 이름 설정
+topic = 'sufka-topic-1'
+
+# Kafka Producer 설정
+producer = Producer({'bootstrap.servers': os.getenv("KAFKA_SERVER")})
+
+# 메시지 전송 콜백 함수
+def delivery_callback(err, msg):
+    if err is not None:
+        print(f'메시지 전송 실패: {err}')
+    else:
+        print(f'메시지 전송 성공: {msg.topic()} [{msg.partition()}]')
 
 
 @app.route("/")
@@ -19,26 +38,32 @@ def get_stock_info(stock_name):
     data = stock.info
     return data
 
-# def cron_job():
-#     stock = yf.Ticker('AAPL')
-#
-#     # 모든 주식 정보를 가져옵니다.
-#     data = stock.info
-#     print(data)
-#
-#     # 스케줄러를 실행한 후 1분 뒤에 스케줄러를 중지합니다.
-#     elapsed_time = datetime.now() - start_time
-#     if elapsed_time >= timedelta(minutes=1):
-#         scheduler.shutdown()
-#     return data
 
-# @app.route("/get_live_stock_data/<second>", methods=['GET'])
-# def get_live_stock_data(second):
-#     # 스케줄러 시작 시간을 저장합니다.
-#     start_time = datetime.now()
-#
-#     scheduler.add_job(cron_job, 'interval', seconds=int(second))
-#     scheduler.start()
-#
-#
-#     return render_template('./index.html')
+def kafka_send_stock_info():
+    stock = yf.Ticker('AAPL')
+
+    # 모든 주식 정보를 가져옵니다.
+    data = stock.info
+
+    # 지정 kafka 로 데이터를 보냅니다.
+    producer.produce(topic, value=pickle.dumps(data), callback=delivery_callback)
+    # 모든 메시지 전송 완료 대기
+    producer.flush()
+    return data
+
+
+scheduler = BackgroundScheduler(max_instances=10)
+
+
+# 매 초 마다 카프카에 주식 정보를 하나 씩 보내는 함수를 실행
+@app.route("/kafka/send_stock_info/start", methods=['POST'])
+def kafka_send_stock_info_start():
+    scheduler.start()
+    scheduler.add_job(func=kafka_send_stock_info, trigger="interval", seconds=1, id='kafka-send-stock-info')
+    return "send start"
+
+# 매 초 마다 카프카에 주식 정보를 하나 씩 보내는 함수를 중단
+@app.route("/kafka/send_stock_info/stop", methods=['POST'])
+def kafka_send_stock_info_stop():
+    scheduler.remove_job('kafka-send-stock-info')
+    return "send stop"
